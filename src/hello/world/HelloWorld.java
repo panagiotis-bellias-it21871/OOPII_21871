2da3604ee8a83ada9d8fef945368d26382b455fa
it21871 <61240983+it21871-dev@users.noreply.github.com>
2020-06-17 11:45:52 +0300

    Hello World

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,18 @@
+package hello.world;
+
+/**
+ *
+ * @author it21871
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        
+    }
+    
+}
